c8caab84b830feaba48de75e2c775b155352d9e0
Steve Li <Steve.Li@monitorerp.com>
2021-11-19 16:15:05 +0800

    Temp update

--- src/app/modules/auth/auth-routing.module.ts
+++ src/app/modules/auth/auth-routing.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AuthComponent,
-    children: [],
+    children: [
+      {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
+        path: 'login',
+        component: LoginComponent,
+      },
+    ],
   },
 ];
 
