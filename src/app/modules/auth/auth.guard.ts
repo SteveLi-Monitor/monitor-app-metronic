d408df622ef0337f16cd81e323c08fdf87086136
Steve Li <Steve.Li@monitorerp.com>
2021-11-22 17:40:07 +0800

    Impl jwt login.

---
+++ src/app/modules/auth/auth.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean | UrlTree {
+    this.authService.redirectUrl = state.url;
+
+    if (!this.authService.isSignedIn) {
+      return this.router.parseUrl('/auth/login');
+    }
+
+    return true;
+  }
+}
