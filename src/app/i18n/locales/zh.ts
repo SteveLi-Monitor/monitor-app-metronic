fc4d797635aab15a84db88a506734a629c95be3c
Steve Li <Steve.Li@monitorerp.com>
2021-11-22 12:00:54 +0800

    Add i18n.

---
+++ src/app/i18n/locales/zh.ts
@@ -0,0 +1,17 @@
+export const ZhLocale = {
+  language: 'zh',
+  data: {
+    Auth: {
+      Login: {
+        Label: {
+          Company: '公司',
+          Username: '用户名',
+          Password: '密码',
+        },
+        Button: {
+          SignIn: '登录',
+        },
+      },
+    },
+  },
+};
