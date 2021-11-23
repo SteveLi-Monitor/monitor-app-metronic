efd88660f5d8ce6322bd558feb1131170de743ca
Steve Li <Steve.Li@monitorerp.com>
2021-11-23 14:47:45 +0800

    Update LoginComponent

--- src/app/modules/auth/auth/login/login.component.ts
+++ src/app/modules/auth/auth/login/login.component.ts
@@ -1,34 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Subscription } from 'rxjs';
+import { CommonCommandsClient } from 'src/app/apis/common-commands.service';
+import { AuthService } from '../../auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements OnInit {
-  constructor() {}
+export class LoginComponent implements OnInit, OnDestroy {
+  private subscriptions: Subscription[] = [];
+
+  constructor(
+    private commonCommandsClient: CommonCommandsClient,
+    private authService: AuthService
+  ) {
+    this.getCompanies();
+  }
 
   loginForm = new FormGroup({
-    company: new FormControl(null, {
-      updateOn: 'submit',
-      validators: Validators.required,
-    }),
-    username: new FormControl(null, {
-      updateOn: 'submit',
-      validators: Validators.required,
-    }),
-    password: new FormControl(null, {
-      updateOn: 'submit',
-      validators: Validators.required,
-    }),
+    companyNumber: new FormControl(null, Validators.required),
+    username: new FormControl(null, Validators.required),
+    password: new FormControl(null, Validators.required),
   });
+
+  companies: {
+    companyNumber: string;
+    name: string;
+  }[] = [];
 
   loginFormSubmitAttempt = false;
 
+  isSigningIn = false;
+
+  signInFailed = false;
+
   ngOnInit(): void {}
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((x) => x.unsubscribe());
+  }
 
   onSubmit(): void {
     this.loginFormSubmitAttempt = true;
+    this.signIn();
+  }
+
+  private getCompanies(): void {
+    const subs = this.commonCommandsClient
+      .getMonitorConfiguration()
+      .subscribe((resp) => {
+        this.companies = resp.companies;
+      });
+
+    this.subscriptions.push(subs);
+  }
+
+  private signIn(): void {
+    if (this.loginForm.valid) {
+      this.isSigningIn = true;
+
+      const subs = this.authService.signIn(this.loginForm.value).subscribe({
+        next: () => {
+          console.log('success');
+          this.isSigningIn = false;
+          this.signInFailed = false;
+        },
+        error: (error) => {
+          console.log(error);
+          this.isSigningIn = false;
+          this.signInFailed = true;
+        },
+      });
+
+      this.subscriptions.push(subs);
+    }
   }
 }
