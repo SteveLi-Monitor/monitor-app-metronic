016ef632e47c73a5a060339866fa0660ea1d96cc
Steve Li <Steve.Li@monitorerp.com>
2021-12-15 16:15:26 +0800

    Add aside

--- src/app/modules/layout/layout.module.ts
+++ src/app/modules/layout/layout.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout/layout.component';
+import { AsideComponent } from './layout/aside/aside.component';
 
 @NgModule({
-  declarations: [LayoutComponent],
+  declarations: [LayoutComponent, AsideComponent],
   imports: [CommonModule, LayoutRoutingModule],
 })
 export class LayoutModule {}
