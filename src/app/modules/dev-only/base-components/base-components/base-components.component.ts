5b2f6415bdae253307d88cc736d0bd2c22342c2a
Steve Li <Steve.Li@monitorerp.com>
2021-12-16 17:31:01 +0800

    Add /dev-only/base-components/buttons

---
+++ src/app/modules/dev-only/base-components/base-components/base-components.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-base-components',
+  templateUrl: './base-components.component.html',
+  styleUrls: ['./base-components.component.scss'],
+})
+export class BaseComponentsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
--- src/app/modules/layout/layout-routing.module.ts
+++ src/app/modules/layout/layout-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Routing } from '../routing';
 import { LayoutComponent } from './layout/layout.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children: Routing,
   },
 ];
 
