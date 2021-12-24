110d6a85ec2cbf553be9750a3db1f3e8e9432c02
Steve Li <Steve.Li@monitorerp.com>
2021-12-24 17:38:04 +0800

    Update

--- src/app/modules/dev-only/tables/tables.module.ts
+++ src/app/modules/dev-only/tables/tables.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AgGridModule } from 'ag-grid-angular';
+
 import { TablesRoutingModule } from './tables-routing.module';
 import { TablesComponent } from './tables/tables.component';
 import { AgGridComponent } from './tables/ag-grid/ag-grid.component';
 
 @NgModule({
   declarations: [TablesComponent, AgGridComponent],
-  imports: [CommonModule, TablesRoutingModule],
+  imports: [CommonModule, AgGridModule.withComponents([]), TablesRoutingModule],
 })
 export class TablesModule {}
